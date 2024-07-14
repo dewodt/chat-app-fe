e3a74e5ddb2480fc666b002c1c5cc0ed46273f7a
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-07-15 06:51:15 +0700

    feat: add lint staged.

--- src/lib/schema/sign-in.ts
+++ src/lib/schema/sign-in.ts
@@ -3,11 +3,7 @@ import { z } from 'zod';
 export const signInFormSchema = z.object({
 	username: z
 		.string({ message: 'Username must not be empty' })
-		.regex(new RegExp('^[a-zA-Z0-9_]*$'), {
-			message: 'Username must contain only letters, numbers, and underscores'
-		})
-		.min(3, { message: 'Username must be at least 3 characters long' })
-		.max(20, { message: 'Username must be at most 20 characters long' }),
+		.min(1, { message: 'Username must not be empty' }),
 	password: z
 		.string({ message: 'Password must not be empty' })
 		.min(1, { message: 'Password must not be empty' })
